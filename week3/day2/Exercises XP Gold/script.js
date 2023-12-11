f40dc04d9e2e9dc57237dd18a6757326b9c0918d
Andrei <andrei@rozhavskie.ru>
2023-12-11 18:56:35 +0200

    done

---
+++ week3/day2/Exercises XP Gold/script.js
@@ -0,0 +1,21 @@
+//Exercise 1
+let sentence = ["my","favorite","color","is","orange"];
+console.log(sentence.join(' '));
+
+//Exercise 2
+let str1="mix";
+let str2="pod";
+
+str1 = ("p" + (str1.slice(1)));
+str2= ("m" + (str2.slice(1)));
+
+let str3 = `${str1} ${str2}`;
+
+console.log(str3);
+
+//Exercise 3
+let num1 = parseFloat(prompt("Please enter the first number"));
+let num2= parseFloat(prompt("Please enter the second number"));
+alert(num1 + num2);
+
+
