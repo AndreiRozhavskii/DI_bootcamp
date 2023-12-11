669900e24a9fc05bf3bf7ff3ee91874936011c0c
Andrei <andrei@rozhavskie.ru>
2023-12-11 14:21:38 +0200

    tt

---
+++ week3/day1/day1.js
@@ -0,0 +1,28 @@
+// //console.log
+// console.log("hello world");
+
+// //variables
+// let x = 3;
+// console.log(x);
+
+//camelCase
+// let dayOfTheMonth = 10
+
+//keywords
+// let x=5+6;
+// console.log(x);
+
+//dataTypes
+
+// let addressNumber = "1";
+// let addressStreet = "Aluf Tamari Nehemia";
+// let country = "Israel";
+// let globalAddress = "I live in" + addressStreet + addressNember + "," + "in" + contry;
+// console.log(globalAddress);
+
+// let birthYear=1988;
+// let futureYear=2024;
+// let age=futureYear-birthYear;
+// console.log("I will be " + age + " in " + futureYear);
+
+
