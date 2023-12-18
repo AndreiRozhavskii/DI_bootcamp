8ee4ef9e311b7fc61c0e7bc6ffc69982259c9064
Andrei <andrei@rozhavskie.ru>
2023-12-18 11:24:46 +0200

    new week

---
+++ week4/day1/script.js
@@ -0,0 +1,17 @@
+function myAge(age){
+    let momAge=age*2;
+   
+   return momAge;
+
+}
+let momAge = myAge(10)
+console.log(momAge);
+
+
+// function userInfo(userName,userAge) {
+//     let result=`My name is ${userName}, my age is ${userAge}`;
+//     return result;
+
+// }
+// let girlInfo = userInfo("Sarah", 22);
+// console.log((girlInfo));
