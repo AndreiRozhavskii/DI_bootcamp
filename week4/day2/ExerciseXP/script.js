5a81c8c9cb6a3b70e911a919ed8b21022d77424c
Andrei <andrei@rozhavskie.ru>
2023-12-25 12:17:42 +0200

    4.2

--- week4/day2/ExerciseXP/script.js
+++ week4/day2/ExerciseXP/script.js
@@ -14,46 +14,46 @@ infoAboutPerson("David", 45, "blue");
 
 //Exerxise 2
 
-// function calculateTip(){
-//    let bill=prompt("Put amount of the bill");
+function calculateTip(){
+   let bill=prompt("Put amount of the bill");
    
-//    if (bill<50){
-//     tips=bill*0.2;
-//    }
-//    if(bill>=50 && bill<200){
-//     tips=bill*0.15;
-//    }
-//    if(bill>200) {
-//     tips=bill*0.1;
-//    }
+   if (bill<50){
+    tips=bill*0.2;
+   }
+   if(bill>=50 && bill<200){
+    tips=bill*0.15;
+   }
+   if(bill>200) {
+    tips=bill*0.1;
+   }
    
-//    console.log(tips);
-//    console.log(tips + parseInt(bill));
+   console.log(tips);
+   console.log(tips + parseInt(bill));
 
-// }
-// calculateTip()
+}
+calculateTip()
 
 //Exercise 3
-// let arr=[]
-// let sum=0
-// function isDivisible(divisor){
+let arr=[]
+let sum=0
+function isDivisible(divisor){
 
-//     for (i=0; i<=500; i++){
+    for (i=0; i<=500; i++){
 
-//     if (i % divisor === 0){
-//         arr.push(i)
-//     }
-// if(i % divisor === 0){
-//     sum=i+sum;
-// }
+    if (i % divisor === 0){
+        arr.push(i)
+    }
+if(i % divisor === 0){
+    sum=i+sum;
+}
     
     
-// }
+}
 
-// }
-// isDivisible(23)
-// console.log(arr.join(' '));
-// console.log(sum);
+}
+isDivisible(23)
+console.log(arr.join(' '));
+console.log(sum);
 
 //Exercise 4
 
@@ -93,12 +93,12 @@ function myBill(){
 myBill()
 
 //Exercise 5
-let values=[.25, .1, .05, .01];
+// let values=[.25, .1, .05, .01];
     
-function changeEnough(itemPrice, amountOfChange){
-    let x=itemPrice;
-    for(let i=0; i<amountOfChange.length; i++){
+// function changeEnough(itemPrice, amountOfChange){
+//     let x=itemPrice;
+//     for(let i=0; i<amountOfChange.length; i++){
     
-    }
-}
+//     }
+// }
 //Exercise 6
