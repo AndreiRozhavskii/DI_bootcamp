c3477b9a71c90e244ead473abdf5c237a39d620e
Andrei <andrei@rozhavskie.ru>
2023-12-26 17:11:37 +0200

    04.4

--- week4/day5/DailyChallenge/script.js
+++ week4/day5/DailyChallenge/script.js
@@ -1,21 +1,21 @@
-const word="bottle";
-const words="bottles";
+
 let count = Number(prompt("Put number of bottles"));
 let i=1;
 while (count > 0){
 
-console.log(count + " " + words + " of beer on the wall ");
-console.log(count + " " + words + " of beer! ");
+console.log(count + " bottles of beer on the wall ");
+console.log(count + " bottles of beer! ");
 count = count - i;
+i=i+1;
 if(count<0){
-    console.log("Take" + (i+1) + " one down, pass them around ");
+    console.log("Take " + (i) + " one down, pass them around ");
     console.log("No bottles of beer on the wall ");
 }
 else if (i===1){
     console.log("Take " + i + " one down, pass it around ");
 }
 else{
-    console.log("Take" + i + " one down, pass them around ");
+    console.log("Take " + i + " one down, pass them around ");
 }
 }
 
--- week4/day5/ExerciseXP/script.js
+++ week4/day5/ExerciseXP/script.js
@@ -1,59 +1,52 @@
 function playTheGame(){
     let usrAns= "Hi! Want to play?"
     if  (confirm(usrAns) == true){
+        let compNum=Math.floor(Math.random() * 10) + 1;
         let usrNum=Number(prompt("Enter the number from 1 up to 10"))
-        if (typeof usrNum !=="number"){
-            alert("Sorry Not a Number, bye!")
-        }
+                
+        console.log(compNum, usrNum);
+        
         if(usrNum<0 || usrNum>10){
             alert("Sorry, out of the area")
         }
         else{
-            const compNum=Math.floor(Math.random() * 10) + 1; 
-            if(usrNum==compNum){
-                        alert("WINNER")
-                    }
-                    else {       
-                        while(usrNum!==compNum){
-                        if(usrNum<compNum){
-                            usrNum=Number(promt("Your number is smaller then the computer’s, guess again"))
-                            }
-                        if(usrNum>compNum){
-                            usrNum=Number(promt("Your number is bigger then the computer’s, guess again"))
-                        }   
-                        
-                
-                    }
-                }
-                  
-        //    compareNumbers(usrNum,compNum);
-       }
-     }
- }
-
+            {
+            compareNumbers(usrNum, compNum)
+            }
+        }
+    }
+    else{
+            alert('No problem, Goodbye');
+        }
+    }
 playTheGame()
 
-// function compareNumbers(usrNum, compNum){
+function compareNumbers(usrNum, compNum){
+    let i=0;
+       
+        console.log(compNum, usrNum);   
+        while(i<2){
+            
+       
+        if(usrNum===compNum){
+            alert("WINNER")
+            return
+        }
+
+        else if(usrNum>compNum){
+                usrNum=Number(prompt(`Your number is bigger then the computer’s, guess again`));
+                console.log(compNum, usrNum);}
+                
+        else if(usrNum<compNum){
+                usrNum=Number(prompt(`Your number is smaller then the computer’s, guess again`))
+                console.log(compNum, usrNum);
+            }
+                i++;
+        }
+    alert("out of chances")
     
-//     if(usrNum==compNum){
-//         alert("WINNER")
-//     }
-//     else {       
-//         while(usrNum!==compNum){
-//         if(usrNum<compNum){
-//             Number(promt("Your number is smaller then the computer’s, guess again"))
-//             }
-//         if(usrNum>compNum){
-//             Number(promt("Your number is bigger then the computer’s, guess again"))
-//         }   
-        
 
-//     }
-// }
-  
-
-
+}  
    
-   
-// }
-// compareNumbers()
+
+compareNumbers()
